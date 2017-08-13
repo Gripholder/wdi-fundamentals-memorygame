c8790bb88bd8ebb3ec658f3c827f571bf92af969
Mohamed Elkarim <mohdawad2010@hotmail.com>
2017-08-13 16:49:38 -0400

    added game logic

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("JS file is connected to HTML! Woo!")
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+// if (cardTwo == cardFour) {
+// 	result = "You have a match!"
+// } if (cardOne == cardThree) {
+// 	result = "You have a match!"
+// } if else (cardTwo == cardFour) {
+// 	result = "Sorry, try again."
+// } if else (cardOne == cardThree) {
+// 	result = "Sorry, try again."
+// }
